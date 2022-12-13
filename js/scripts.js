2f8ca11c3377809d24a44647762c01c31e3beaa9
dazzxalwx <dnmonte@gmail.com>
2022-12-13 20:55:11 +0000

    Visuals

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+let result = 0
+
+function add(number1, number2) {
+    return result = number1 + number2
+}
+
+function subtractNumber(number1, number2) {
+    return result = number1 - number2
+}
+
+function multiply(number1, number2) {
+    return result = number1 * number2
+}
+
+function divide(number1, number2) {
+    return result = number1 / number2
+}
+
+function operate(number1, number2) {
+    add(number1, number2)
+}
